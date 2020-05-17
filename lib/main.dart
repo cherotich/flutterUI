a29cde1de77b397b48f11bf4325a2f0cab3eedda
cherotich <carolcaro60@gmail.com>
2020-05-17 12:35:17 +0300

    Login page

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,14 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
-       
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
@@ -23,85 +20,132 @@ debugShowCheckedModeBanner: false,
   }
 }
 
-class MyHomePage  extends StatelessWidget {
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-     body:Center(
-     
-       child: Column(
-           mainAxisAlignment:MainAxisAlignment.center,
-         children: <Widget>[
-           new Stack(
-             alignment: Alignment.center,
-             children: <Widget>[
-               new Container(
-                 height: 60.0,
-                 width: 60.0,
-                 decoration: new BoxDecoration(
-                   borderRadius: new BorderRadius.circular(50.0),
-                   color: Color(0xFF18D191)  
-                  
-                 ), 
-
-
-child: new Icon(Icons.local_offer,color:Colors.white,),          
-           
-             ),
-
-
-     new Container(
-       margin: new EdgeInsets.only(right:50.0,top:50.0),
-                 height: 60.0,
-                 width: 60.0,
-                 decoration: new BoxDecoration(
-                   borderRadius: new BorderRadius.circular(50.0),
-                  color: Color(0xFFFC6A7F)
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                new Container(
+                  height: 60.0,
+                  width: 60.0,
+                  decoration: new BoxDecoration(
+                      borderRadius: new BorderRadius.circular(50.0),
+                      color: Color(0xFF18D191)),
+                  child: new Icon(
+                    Icons.local_offer,
+                    color: Colors.white,
+                  ),
+                ),
+                new Container(
+                  margin: new EdgeInsets.only(right: 50.0, top: 50.0),
+                  height: 60.0,
+                  width: 60.0,
+                  decoration: new BoxDecoration(
+                      borderRadius: new BorderRadius.circular(50.0),
+                      color: Color(0xFFFC6A7F)),
+                  child: new Icon(
+                    Icons.home,
+                    color: Colors.white,
+                  ),
+                ),
+                new Container(
+                  margin: new EdgeInsets.only(left: 20.0, top: 50.0),
+                  height: 60.0,
+                  width: 60.0,
+                  decoration: new BoxDecoration(
+                      borderRadius: new BorderRadius.circular(50.0),
+                      color: Color(0xFFFFCE56)),
+                  child: new Icon(
+                    Icons.local_car_wash,
+                    color: Colors.white,
+                  ),
+                ),
+                new Container(
+                  margin: new EdgeInsets.only(left: 80.0, top: 40.0),
+                  height: 60.0,
+                  width: 60.0,
+                  decoration: new BoxDecoration(
+                      borderRadius: new BorderRadius.circular(50.0),
+                      color: Color(0xFF45E0EC)),
+                  child: new Icon(
+                    Icons.place,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.only(top:8.0,bottom: 80.0),
+                    child: new Text(
+                      "Quick Bee",
+                      style: new TextStyle(fontSize: 30.0),
+                    ))
+              ],
+            ),
+            new Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0,right: 10.0,top: 10.0),
+                    child: new Container(
+                      alignment: Alignment.center,
+                      height: 60.0,
+                      decoration: new BoxDecoration(color: Color(0xFF18D191),borderRadius: new BorderRadius.circular(10.0)),
+                 child: new Text("Sign in with Email",style: new TextStyle(fontSize:20.0,color: Colors.white),
                  ),
+                    ),
+                  ),
+                )
+              ],
 
 
-child: new Icon(Icons.home,color:Colors.white,),          
-           
 
-             ),
-
-    
-       new Container(
-       margin: new EdgeInsets.only(left:30.0,top:50.0),
-                 height: 60.0,
-                 width: 60.0,
-                 decoration: new BoxDecoration(
-                   borderRadius: new BorderRadius.circular(50.0),
-                  color: Color(0xFFFFCE56)
-                 ),
-
-
-child: new Icon(Icons.local_car_wash,color:Colors.white,),          
-           
-           
-             ),
-
-
-     new Container(
-       margin: new EdgeInsets.only(left:60.0,top:40.0),
-                 height: 60.0,
-                 width: 60.0,
-                 decoration: new BoxDecoration(
-                   borderRadius: new BorderRadius.circular(50.0),
-                  color: Color(0xFF45E0EC)
-                 ),
-
-
-child: new Icon(Icons.place,color:Colors.white,),          
-           
-           
-             )
-             ],
-           )
+            ),
          
-         ],
-       ),
-       ),
+                     new Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0,right: 5.0,top: 10.0),
+                    child: new Container(
+                      alignment: Alignment.center,
+                      height: 60.0,
+                      decoration: new BoxDecoration(color: Color(0xFF4364A1),borderRadius: new BorderRadius.circular(10.0)),
+                 child: new Text("FaceBook",style: new TextStyle(fontSize:20.0,color: Colors.white),
+                 ),
+                    ),
+                  ),
+                ),
+         Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10.0,right: 10.0,top: 10.0),
+                    child: new Container(
+                      alignment: Alignment.center,
+                      height: 60.0,
+                      decoration: new BoxDecoration(color: Color(0xFFDF513B),borderRadius: new BorderRadius.circular(10.0)),
+                 child: new Text("Google",style: new TextStyle(fontSize:20.0,color: Colors.white),
+                 ),
+                    ),
+                  ),
+                )
+
+              ],
+
+              
+            ),
+         
+          ],
+        ),
+      ),
     );
   }
 }
