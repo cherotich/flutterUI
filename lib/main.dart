f26190b254bba10fc71c6b7f6a3b4b412e851f23
cherotich <carolcaro60@gmail.com>
2020-05-17 14:01:49 +0300

    Added more login UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'login.dart';
 
 void main() {
   runApp(MyApp());
@@ -79,70 +80,92 @@ class MyHomePage extends StatelessWidget {
                   ),
                 )
               ],
+            
+            
             ),
             new Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Padding(
-                    padding: const EdgeInsets.only(top:8.0,bottom: 80.0),
+                    padding: const EdgeInsets.only(top: 8.0, bottom: 80.0),
                     child: new Text(
                       "Quick Bee",
                       style: new TextStyle(fontSize: 30.0),
                     ))
+              ],
+          
+          
+          
+            ),
+            new Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 20.0, right: 10.0, top: 10.0),
+                    child: GestureDetector(
+                      onTap: () {
+
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginPage(),
+                        ));
+                      },
+                      child: new Container(
+                        alignment: Alignment.center,
+                        height: 60.0,
+                        decoration: new BoxDecoration(
+                            color: Color(0xFF18D191),
+                            borderRadius: new BorderRadius.circular(10.0)),
+                        child: new Text(
+                          "Sign in with Email",
+                          style: new TextStyle(
+                              fontSize: 20.0, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
               ],
             ),
             new Row(
               children: <Widget>[
                 Expanded(
                   child: Padding(
-                    padding: const EdgeInsets.only(left: 20.0,right: 10.0,top: 10.0),
+                    padding: const EdgeInsets.only(
+                        left: 20.0, right: 5.0, top: 10.0),
                     child: new Container(
                       alignment: Alignment.center,
                       height: 60.0,
-                      decoration: new BoxDecoration(color: Color(0xFF18D191),borderRadius: new BorderRadius.circular(10.0)),
-                 child: new Text("Sign in with Email",style: new TextStyle(fontSize:20.0,color: Colors.white),
-                 ),
-                    ),
-                  ),
-                )
-              ],
-
-
-
-            ),
-         
-                     new Row(
-              children: <Widget>[
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20.0,right: 5.0,top: 10.0),
-                    child: new Container(
-                      alignment: Alignment.center,
-                      height: 60.0,
-                      decoration: new BoxDecoration(color: Color(0xFF4364A1),borderRadius: new BorderRadius.circular(10.0)),
-                 child: new Text("FaceBook",style: new TextStyle(fontSize:20.0,color: Colors.white),
-                 ),
+                      decoration: new BoxDecoration(
+                          color: Color(0xFF4364A1),
+                          borderRadius: new BorderRadius.circular(10.0)),
+                      child: new Text(
+                        "FaceBook",
+                        style:
+                            new TextStyle(fontSize: 20.0, color: Colors.white),
+                      ),
                     ),
                   ),
                 ),
-         Expanded(
+                Expanded(
                   child: Padding(
-                    padding: const EdgeInsets.only(left: 10.0,right: 10.0,top: 10.0),
+                    padding: const EdgeInsets.only(
+                        left: 10.0, right: 10.0, top: 10.0),
                     child: new Container(
                       alignment: Alignment.center,
                       height: 60.0,
-                      decoration: new BoxDecoration(color: Color(0xFFDF513B),borderRadius: new BorderRadius.circular(10.0)),
-                 child: new Text("Google",style: new TextStyle(fontSize:20.0,color: Colors.white),
-                 ),
+                      decoration: new BoxDecoration(
+                          color: Color(0xFFDF513B),
+                          borderRadius: new BorderRadius.circular(10.0)),
+                      child: new Text(
+                        "Google",
+                        style:
+                            new TextStyle(fontSize: 20.0, color: Colors.white),
+                      ),
                     ),
                   ),
                 )
-
               ],
-
-              
             ),
-         
           ],
         ),
       ),
